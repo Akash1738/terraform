a0256a07dfb750fdecf1011f8aa0e19d75a392d3
Akash1738 <akashbajgire12@gmail.com>
2026-01-31 11:43:38 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "alb_name" {
+  description = "Name of the ALB"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
+}
+
+variable "public_subnets" {
+  description = "List of public subnet IDs"
+  type        = list(string)
+}
