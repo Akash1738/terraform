ebf67529e1f6d1454505ed64c12f07bc847b1be7
Akash1738 <akashbajgire12@gmail.com>
2026-01-31 13:08:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 # Security Group for ALB
 resource "aws_security_group" "alb_sg" {
-  name        = "${var.alb_name}-sg"
+  name        = "${alb_name}-sg"
   description = "Allow HTTP inbound traffic"
-  vpc_id      = var.vpc-0add612466eab90e5
+  vpc_id      = vpc-0add612466eab90e5
 
   ingress {
     from_port   = 80
@@ -35,10 +35,10 @@ resource "aws_lb" "alb" {
 
 # Target Group
 resource "aws_lb_target_group" "tg" {
-  name     = "${var.alb_name}-tg"
+  name     = "${alb_name}-tg"
   port     = 80
   protocol = "HTTP"
-  vpc_id   = var.vpc_id
+  vpc_id   = vpc-0add612466eab90e5
 
   health_check {
     path                = "/"
