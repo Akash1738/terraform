6c7167233c39c3cfc0db1ffff2feb69bb38dcc89
Akash1738 <akashbajgire12@gmail.com>
2026-01-31 13:17:25 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,21 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "alb_name" {
+  description = "alb"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "VPC ID where ALB will be created"
+  type        = string
+}
+
+variable "public_subnets" {
+  description = "List of public subnet IDs"
+  type        = list(string)
+}
 
