f4756bfb5243ce29500394d4ef293d732c17a510
@kki... <akashbajgire12@gmail.com>
2026-02-01 12:52:03 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "akash-s3-bucket200"   # replace with your unique bucket name
-    key            = "global/terraform.tfstate"       # path inside the bucket
-    region         = "ap-south-1"                     # your AWS region
-    #dynamodb_table = "terraform-locks"                # DynamoDB table for state locking
-    encrypt        = true                             # enable server-side encryption
+    bucket         = "akash-s3-bucket200"   
+    key            = "global/terraform.tfstate"       
+    region         = "ap-south-1"                     
+    # dynamodb_table = "terraform-locks"                
+    encrypt        = true                             
   }
 }
